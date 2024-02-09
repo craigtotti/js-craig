89677b8ad6dde31141c115874b4ed521bbed19e9
Craig Faria <111559108+craigtotti@users.noreply.github.com>
2024-02-09 12:48:56 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+let num = 10;
+
+let str = "hello world";
+
+let bool = true;
+ 
+let nul = null ;
+
+let und = "undefined";
+
+let obj = {name:"craig", age:25};
+
+let arr =[1,2,3,4];
+
+
+let sym =Symbol("foo");
+
+function greet(name) {
+    console.log("hello" + name + "!");
+}
+
+
+
+
+console.log("Number:", num);
+console.log("String:", str);
+console.log("Boolean:", bool);
+console.log("Null:", nul);
+console.log("Undefined:", und);
+console.log("Object:", obj);
+console.log("Array:", arr);
+console.log("Function:", greet);
+console.log("Symbol:", sym);
+
+
+console.log(typeof name);
